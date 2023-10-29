d8bd2cc06341d6d3d7f2b496ad6f0929062481c4
Juan Ángel <ursk445@gmail.com>
2023-10-28 22:35:35 -0500

    Se agrega el SeederDB.

---
+++ API_First_Proyect/DAL/SeederDB.cs
@@ -0,0 +1,69 @@
+﻿using API_First_Proyect.DAL.Entities;
+using API_First_Proyect.DAL.Entitites;
+
+namespace API_First_Proyect.DAL
+{
+     public class SeederDB
+     {
+          private readonly DataBaseContext _context;
+
+          public SeederDB(DataBaseContext context)
+          {
+               _context = context;
+          }
+
+          // Creamos el método "SeederAsync" (Funciona como un método MAIN para inicializar las tablas de la BD)
+          public async Task SeederAsync()
+          {
+               // Agregar método propio de EF que hace la función del comando "update-database".
+               await _context.Database.EnsureCreatedAsync();
+
+               await PopulateCountriesAsync();
+               await _context.SaveChangesAsync();
+          }
+
+          #region "Private Methods"
+
+          private async Task PopulateCountriesAsync()
+          {
+               if (!_context.Countries.Any())
+               {
+                    _context.Countries.Add(new Country
+                    {
+                         CreatedDate = DateTime.Now,
+                         Name = "Colombia",
+                         States = new List<State>()
+                         {
+                              new State
+                              {
+                                   CreatedDate = DateTime.Now,
+                                   Name = "Antioquia"
+                              },
+
+                              new State
+                              {
+                                   CreatedDate = DateTime.Now,
+                                   Name = "Cundinamarca"
+                              }
+                         }
+                    });
+
+                    _context.Countries.Add(new Country
+                    {
+                         CreatedDate = DateTime.Now,
+                         Name = "Argentina",
+                         States = new List<State>()
+                         {
+                              new State
+                              {
+                                   CreatedDate = DateTime.Now,
+                                   Name = "Buenos Aires"
+                              },
+                         }
+                    });
+               }
+          }
+
+          #endregion
+     }
+}
--- API_First_Proyect/Program.cs
+++ API_First_Proyect/Program.cs
@@ -12,14 +12,29 @@ builder.Services.AddControllers();
 // Esta linea me crea el contexto de la BD a la hora de correr esta API.
 builder.Services.AddDbContext<DataBaseContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Agregamos la dependencia para conectar el servicio "Country" a la base de datos.
+// Agregamos la dependencia para conectar el servicio "Country y State" a la base de datos.
 builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddScoped<IStateService, StateService>();
+builder.Services.AddTransient<SeederDB>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+SeederData();
+
+void SeederData()
+{
+     IServiceScopeFactory? scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+
+     using (IServiceScope? scope = scopedFactory.CreateScope())
+     {
+          SeederDB? service = scope.ServiceProvider.GetService<SeederDB>();
+          service.SeederAsync().Wait();
+     }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
